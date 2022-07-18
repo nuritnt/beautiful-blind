3ea1976e54330b4cd7a66fe02609c0b6bc7e3205
Tuğçe Nur Taş <tnt@panter.ch>
2022-07-18 13:34:07 +0200

    get release data from prismic cms

--- src/components/Release.js
+++ src/components/Release.js
@@ -2,23 +2,30 @@ import "./Release.scss";
 import releaseImage from "../assets/images/enton.jpg";
 import Socials from "./Socials";
 import releaseIcons from "../assets/releaseIcons.json";
+import { useSinglePrismicDocument } from "@prismicio/react";
 
 const Release = () => {
+  const [latest_release] = useSinglePrismicDocument("latest_release");
   return (
-    <div className="content release centered">
-      <img
-        src={releaseImage}
-        alt="Potrait of Enton"
-        className="release__image"
-      />
-      <h2 className="headline headline--large">Die letzte Rose</h2>
-      <p className="small-hr">
-        Das neue Album
-        <br />
-        12. November 2022
-      </p>
-      <Socials icons={releaseIcons.icons} />
-    </div>
+    <>
+      {latest_release && (
+        <div className="content release centered">
+          {/* {console.log(latest_release)} */}
+          <img
+            src={latest_release.data.image.url}
+            alt={latest_release.data.image_alt_text[0].text}
+            className="release__image"
+          />
+          <h2 className="headline headline--large">
+            {latest_release.data.title[0].text}
+          </h2>
+          <p className="small-hr release__description">
+            {latest_release.data.description}
+          </p>
+          <Socials icons={releaseIcons.icons} />
+        </div>
+      )}
+    </>
   );
 };
 
