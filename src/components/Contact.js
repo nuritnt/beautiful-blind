157c1ca069441764f1de2ab731775528aab7d89e
Tuğçe Nur Taş <tnt@panter.ch>
2022-07-18 13:34:45 +0200

    get contact data from prismic cms

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,41 @@
 import "./Contact.scss";
+import { useSinglePrismicDocument } from "@prismicio/react";
 
 const Contact = () => {
+  const [contact] = useSinglePrismicDocument("contact");
+
   return (
-    <div className="content content--dual contact">
-      <div className="content__col">
-        <h2 className="headline headline--small">Kontakt</h2>
-        <a className="cta" href="mailto:https://google.com">
-          info@fabian.ch
-        </a>
-      </div>
-      <div className="content__col">
-        <h2 className="headline headline--small">Medien</h2>
-        <a className="cta" href="https://google.com">
-          Download Pressefotos
-        </a>
-      </div>
-    </div>
+    <>
+      {contact && (
+        <div className="content content--dual contact">
+          <div className="content__col">
+            <h2 className="headline headline--small">Kontakt</h2>
+            <div className="cta-wrap">
+              {contact.data.contact_links.map((link, index) => (
+                <a className="cta" href={link.link_url.url} key={index}>
+                  {link.link_title}
+                </a>
+              ))}
+            </div>
+          </div>
+          <div className="content__col">
+            <h2 className="headline headline--small">Medien</h2>
+            <div className="cta-wrap">
+              {contact.data.media_links.map((link, index) => (
+                <a
+                  className="cta"
+                  download
+                  href={link.media_url.url}
+                  key={index}
+                >
+                  {link.link_title}
+                </a>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
